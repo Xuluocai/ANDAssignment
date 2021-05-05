8f3d35c021488a9ec3a6fa8a65ffb3c1e2a3db66
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2021-05-06 00:15:11 +0200

    Update MainActivityViewModel.java

--- app/src/main/java/com/example/and_assignment/ui/MainActivityViewModel.java
+++ app/src/main/java/com/example/and_assignment/ui/MainActivityViewModel.java
@@ -5,14 +5,17 @@ import android.app.Application;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
+import com.example.and_assignment.data.Flight;
 import com.example.and_assignment.data.UserRepository;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivityViewModel extends AndroidViewModel {
     private final UserRepository userRepository;
+    private Flight flight;
     public MainActivityViewModel(Application app){
         super(app);
         userRepository = UserRepository.getInstance(app);
+        flight=new Flight();
 
     }
     public void init() {
@@ -25,6 +28,21 @@ public class MainActivityViewModel extends AndroidViewModel {
 
     public void signOut() {
         userRepository.signOut();
+    }
+
+    public void setDeparture(String departure){
+        flight.setDeparture(departure);
+    }
+    public void setArrival(String arrival){
+        flight.setArrival(arrival);
+    }
+
+    public String getDeparture(){
+        return flight.getDeparture();
+    }
+
+    public String getArrival(){
+        return flight.getArrival();
     }
 
 

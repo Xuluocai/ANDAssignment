285fe2f7acfa1363c835014bc0b2e015001df12b
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2021-05-06 00:14:46 +0200

    Update ServiceGenerator.java

--- app/src/main/java/com/example/and_assignment/data/ServiceGenerator.java
+++ app/src/main/java/com/example/and_assignment/data/ServiceGenerator.java
@@ -1,16 +1,15 @@
 package com.example.and_assignment.data;
 
-import retrofit2.Retrofit;
 public class ServiceGenerator {
     private static FlightApi flightApi;
 
-    public static FlightApi getFlightApi(){
-        if(flightApi==null){
-            flightApi=new Retrofit.Builder()
-                    .baseUrl()
-                    .addConverterFactory()
-                    .build()
-                    .creat(FlightApi.class);
-        }return flightApi;
-    }
+   // public static FlightApi getFlightApi(){
+       // if(flightApi==null){
+      //      flightApi=new Retrofit.Builder()
+                   // .baseUrl()
+             //       .addConverterFactory()
+            //        .build()
+               //     .creat(FlightApi.class);
+     //   }return flightApi;
+  //  }
 }

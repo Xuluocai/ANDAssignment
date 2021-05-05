5bbf4950cb0d311e35dfcc2faaf3e5d787caffea
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2021-05-06 00:15:05 +0200

    Update FlightRepository.java

--- app/src/main/java/com/example/and_assignment/data/FlightRepository.java
+++ app/src/main/java/com/example/and_assignment/data/FlightRepository.java
@@ -1,13 +1,9 @@
 package com.example.and_assignment.data;
 
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.internal.EverythingIsNonNull;
 
@@ -29,22 +25,22 @@ public class FlightRepository {
         return flightMutableLiveData;
     }
 
-public void setFlight(String flghtNumber){
-        FlightApi flightApi=ServiceGenerator.getFlightApi();
-        Call<FlightResponse> call =flightApi.getFlight(flghtNumber);
-    call.enqueue(new Callback<FlightResponse>() {
+//public void setFlight(String flghtNumber){
+   //     FlightApi flightApi=ServiceGenerator.getFlightApi();
+   //     Call<FlightResponse> call =flightApi.getFlight(flghtNumber);
+  //  call.enqueue(new Callback<FlightResponse>() {
         @EverythingIsNonNull
-        @Override
+   //     @Override
         public void onResponse(Call<FlightResponse> call, Response<FlightResponse> response) {
             if(response.isSuccessful())
                 flightMutableLiveData.setValue(response.body().getFlight());
         }
 
-        @EverythingIsNonNull
-            @Override
-            public void onFailure(Call<FlightResponse> call,Throwable t){
-                Log.i("Retrofit","Something went wrong");
-            }
-        });
-}
+      //  @EverythingIsNonNull
+      //      @Override
+        //    public void onFailure(Call<FlightResponse> call,Throwable t){
+       //         Log.i("Retrofit","Something went wrong");
+        //    }
+      //  });
+//}//
 }
